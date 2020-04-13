cd02aef447c14e0d1a105c6a9ad904178032cf08
Jhondah <joehondah@googlemail.com>
2020-04-13 20:51:53 +0200

    request links comments

--- API/Main.py
+++ API/Main.py
@@ -1,3 +1,13 @@
 import requests
 
 # requests.get()
+
+
+# api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
+# api.openweathermap.org/data/2.5/weather?id={city id}&appid={your api key}
+
+#api.openweathermap.org/data/2.5/find?q=London&units=metric&appid={75a90db613d4fa920dd60f4bb3be02ef}
+
+#http://api.openweathermap.org/data/2.5/find?q=Frankfurt&units=metric&appid=75a90db613d4fa920dd60f4bb3be02ef&mode=xml
+
+#http://api.openweathermap.org/data/2.5/box/city?bbox=12,32,15,37,10&units=metric&appid=75a90db613d4fa920dd60f4bb3be02ef&mode=xml
--- Launch.py
+++ Launch.py
@@ -7,6 +7,8 @@ API_KEYS = {
     "OpenWeatherMap": "75a90db613d4fa920dd60f4bb3be02ef"
 }
 
+
+
 # start api worker
 # ApiMain.
 
