427277c7b5014630c2ef2803356b88e659ddd9c0
BELTOL3011 <bela.tollking@gmail.com>
2020-04-13 20:44:12 +0200

    modified Launch, API/Main.py

--- GUI/Main.py
+++ GUI/Main.py
@@ -1,11 +1,21 @@
 from tkinter import *
 
 
+def mainloop():
+    if not root:
+        raise Exception("[GUI] No main Window created.")
+
+    while 1:
+        root.update()
+
+
 def start():
     global root
 
     root = Tk()
     root.title("WeatherViewer by JHondah and Belissimo")
+    print(type(root))
+    mainloop()
 
 
-root = None
+root: Tk
