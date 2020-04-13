db29be992173d756541ab8acc5b6e37c8bd533bf
Jhondah <joehondah@googlemail.com>
2020-04-13 21:28:46 +0200

    request function

--- API/Main.py
+++ API/Main.py
@@ -2,12 +2,35 @@ import requests
 
 # requests.get()
 
+openweather_main_url = "https://api.openweathermap.org/data/2.5/weather?"
+openweather_appID = "75a90db613d4fa920dd60f4bb3be02ef"
+
+
+def build_request_string(bodystring: str, appid: str, cityname: str, country: str, XML: bool):
+
+# country: de, uk, usa  ...
 
 # api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
 # api.openweathermap.org/data/2.5/weather?id={city id}&appid={your api key}
+# api.openweathermap.org/data/2.5/find?q=London&units=metric&appid=75a90db613d4fa920dd60f4bb3be02ef
+# http://api.openweathermap.org/data/2.5/find?q=Frankfurt&units=metric&appid=75a90db613d4fa920dd60f4bb3be02ef&mode=xml
+# http://api.openweathermap.org/data/2.5/box/city?bbox=12,32,15,37,10&units=metric&appid=75a90db613d4fa920dd60f4bb3be02ef&mode=xml
+# https://openweathermap.org/current#data
 
-#api.openweathermap.org/data/2.5/find?q=London&units=metric&appid={75a90db613d4fa920dd60f4bb3be02ef}
+    get_string = bodystring + "q="+ cityname + "," + country +  "&appid=" + appid
+    get_string = get_string + "&units=metric"   #hardcoded
+    get_string = get_string + "&lang=de"        #hardcoded
 
-#http://api.openweathermap.org/data/2.5/find?q=Frankfurt&units=metric&appid=75a90db613d4fa920dd60f4bb3be02ef&mode=xml
+    if XML:
+        get_string = get_string + "&mode=xml"
 
-#http://api.openweathermap.org/data/2.5/box/city?bbox=12,32,15,37,10&units=metric&appid=75a90db613d4fa920dd60f4bb3be02ef&mode=xml
+    print(get_string)
+    back = requests.get(get_string)
+    print(back)
+
+
+
+
+build_request_string(openweather_main_url, openweather_appID, "Berlin", "de", True)
+
+
